49f566b80245ce9fff87ca4f027215073ec19248
Clive Smith <clive.smith.e25590@mcast.edu.mt>
2022-02-05 20:02:51 +0100

    first commit

---
+++ doc/Programs/CarSales-AutoArima.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thur Jan 21 02:50 2022
+
+@author: Clive Smith
+"""
+
+import pandas as pd
+from matplotlib import pyplot
+from statsmodels.tsa.seasonal import seasonal_decompose
+from pmdarima import auto_arima
+import numpy as np
+
+
+#Reads from xlxs file and puts in new dataframe
+df = pd.read_excel("C:\\Users\\Csmit\\Documents\\MCAST\\Third Year\\Dissertation\\Dissertation\\dissertation-clive-smith\\doc\\Dataset\\CarSalesAuto.xlsx")
+
+#Puts data with category = 'Furniture' into new Dataframe
+CarDF = df.loc[df['Vehicle_type'] == 'Car']
+
+#Shows min and max of dates from rows
+CarDF['Launch'].min(), CarDF['Launch'].max()
+
+#Name unneeded columns from original df and removes them from furniture dataframe
+cols = ['Manufacturer_model', 'Price', 'Horsepower', 'Fuel_efficiency', 'Vehicle_type']
+CarDF.drop(cols, axis=1, inplace=True)
+
+#Sorts values
+CarDF = CarDF.sort_values('Launch')
+#Checks null rows
+CarDF.isnull().sum()
+
+#Groups rows with same date and sums their sales figure up
+CarDF = CarDF.groupby('Launch')['Sales'].sum().reset_index()
+
+#Sets index as their date(timestamp)
+CarDF = CarDF.set_index('Launch')
+CarDF.index
+
+#Takes the average sale figure of each month to make it as a MONTHLY time series and shows
+y = CarDF['Sales'].resample('MS').mean()
+y['2019':]
+
+#Decompose to show trend, seasonality, residual and observed graphs
+decomposition = seasonal_decompose(y, model = 'additive')
+decomposition.plot()
+pyplot.show()
+
+#This library contains an auto_arima function that allows us to set a range of p,d,q,P,D,and Q values and then fit models for all the possible combinations. Then the model will keep the combination that reported back the best AIC value.
+stepwise_model = auto_arima(y, start_p = 1, start_q = 1,
+                            max_p = 3, max_q = 3, m = 12,
+                            start_P = 0, seasonal = True,
+                            d = 1, D = 1, trace = True,
+                            error_action = 'ignore', 
+                            supress_warnings = True, 
+                            stepwise = True)
+
+#The Akaike information criterion (AIC) is an estimator of the relative quality of statistical models for a given set of data. Given a collection of models for the data, AIC estimates the quality of each model, relative to each of the other models.
+#In this case the lowest AIC is 486.2063393665944
+print(stepwise_model.aic())
+
+#Split into training and testing datasets
+train = y.loc['2010-02-01':'2019-10-01']
+test = y.loc['2019-11-01':]
+
+#Fits ARIMA model to training data
+stepwise_model.fit(train)
+
+#Predicts 2 periods + 36 new values (Test dataset number of rows)
+test_forecast = stepwise_model.predict(n_periods=2)
+future_forecast = stepwise_model.predict(n_periods=36)
+
+#Concats predictions to test dataset rows into dataframe -- needs to be the size of the test dataset
+test_forecast = pd.DataFrame(test_forecast,index = test.index,columns=['Prediction'])
+
+#Frequency MS means month start
+new = pd.DataFrame(future_forecast,index = pd.date_range(start='2020-01-01', end='2022-12-01', freq='MS'),columns=['Prediction'])
+
+#Shows the mean squared error
+mse = ((test_forecast['Prediction'] - test[1]) ** 2).mean()
+print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+#Shows the RMSE - In this case - 420.08
+print('The Root Mean Squared Error of our forecasts is {}'.format(round(np.sqrt(mse), 2)))
+
+pd.concat([y,test_forecast],axis=1).plot()
+
+#Concats and plots
+pd.concat([test,test_forecast],axis=1).plot()
+
+#Concats and plots with training dataset (whole dataset)
+pd.concat([y,new],axis=1).plot()
+
+print(stepwise_model.summary())
---
+++ doc/Programs/CarSales-GridSearch.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 19 18:50 2022
+
+@author: Clive Smith
+"""
+
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+
+print(np.__version__)
+print(pd.__version__)
+print(sm.__version__)
+print(matplotlib.__version__)
+
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'black'
+
+#Reads from xlxs file and puts in new dataframe
+df = pd.read_excel("C:\\Users\\Csmit\\Documents\\MCAST\\Third Year\\Dissertation\\Dissertation\\dissertation-clive-smith\\doc\\Dataset\\CarSalesGrid.xlsx")
+
+#Shows min and max of dates from rows
+df['DateObserved'].min(), df['DateObserved'].max()
+
+#Sorts values by observation_df
+df = df.sort_values('DateObserved')
+
+#Checks null rows
+df.isnull().sum()
+
+#Groups rows with same date and sums their sales figure up
+df = df.groupby('DateObserved')['Sales'].sum().reset_index()
+
+#Sets index as their date(timestamp)
+df = df.set_index('DateObserved')
+df.index
+
+#Takes the average sale figure of each month to make it as a MONTHLY time series and shows
+y = df['Sales'].resample('MS').mean()
+y['2017':]
+
+#Visualise current data
+y.plot(figsize=(15, 6))
+plt.show()
+
+#Decomposing time series into trend, seasonality and noise
+from pylab import rcParams
+rcParams['figure.figsize'] = 18, 8
+decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+fig = decomposition.plot()
+plt.show()
+
+#Creates all possible combinations for SARIMAX in pdq combined with seasonal_pdq
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+print('Examples of parameter combinations for Seasonal ARIMA...')
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+#Performs grid search to find the best possible parameters for the model
+for param in pdq:
+    for param_seasonal in seasonal_pdq:
+        try:
+            mod = sm.tsa.statespace.SARIMAX(y,
+                                            order=param,
+                                            seasonal_order=param_seasonal,
+                                            enforce_stationarity=False,
+                                            enforce_invertibility=False)
+            results = mod.fit()
+            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+        except:
+            continue
+        
+        #Best model results with params (1,1,1)x(0,0,1,12)12- AIC: 993.8
+#Therefore we will fit this model as it shows that its the best one    
+mod = sm.tsa.statespace.SARIMAX(y,
+                    order=(1, 1, 1),
+                    seasonal_order=(1, 1, 1, 12),
+                    enforce_stationarity=False,
+                    enforce_invertibility=False)
+results = mod.fit()
+print(results.summary().tables[1])
+
+#Plotting the diagnostics for any unusual behavior
+results.plot_diagnostics(figsize=(16, 8))
+plt.show()
+#These plots show that residuals are near normally distributed 
+
+#We will test the model forecasts by comparing real data to forecasted one from 2017 to end of dataset
+#Afterwards we will plot to see the difference
+pred = results.get_prediction(start=pd.to_datetime('2014-10-01'), dynamic=False)
+pred_ci = pred.conf_int()
+ax = y['2014':].plot(label='observed')
+pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
+
+#This plots the confidence interval of the fitted parameters.
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.2)
+ax.set_xlabel('Date')
+ax.set_ylabel('Car Sales')
+plt.legend()
+plt.show()
+
+#Shows the mean squared error - 1085.79
+y_forecasted = pred.predicted_mean
+y_truth = y['2014-10-01':]
+mse = ((y_forecasted - y_truth) ** 2).mean()
+print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+
+#Shows the RMSE - Forecast vary by +- 32.95
+print('The Root Mean Squared Error of our forecasts is {}'.format(round(np.sqrt(mse), 2)))
+
+#Forecast for the next 36 months or 3 years
+pred_uc = results.get_forecast(steps=36)
+pred_ci = pred_uc.conf_int()
+ax = y.plot(label='Observed', figsize=(14, 7), color = 'grey')
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast', color = 'green')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('Domestic Vehicle Sales')
+plt.legend()
+plt.show()
